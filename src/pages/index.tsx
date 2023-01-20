d5cab55017fcc05b91dc053258c3163f17533cf2
Bruno <brun0yuj1ym02@gmail.com>
2023-01-20 11:39:16 +0900

    create Login Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-primary">Test</button>
     </>
   );
 }
