f2bbc07a5c6c94fc99910908069394c8b672b9eb
Bruno <brun0yuj1ym02@gmail.com>
2023-01-23 01:46:04 +0900

    update login

--- src/components/login/Form.tsx
+++ src/components/login/Form.tsx
@@ -1,11 +1,19 @@
 import { Checkbox } from '@mui/material';
-import { Eye } from 'phosphor-react';
+import {
+  Eye,
+  FacebookLogo,
+  FaceMask,
+  GoogleChromeLogo,
+  GoogleLogo,
+} from 'phosphor-react';
 import React from 'react';
+
+import Link from 'next/link';
 
 export default function Form() {
   return (
-    <div className="px-5 mt-6">
-      <form className="flex flex-col gap-3">
+    <div className="px-5 mt-6 flex flex-col items-center gap-3">
+      <form className="flex flex-col gap-3 w-full">
         <div className="flex flex-col gap-1">
           <label className="ml-5 text-sm" htmlFor="email">
             Email<span className="text-primary-color">*</span>
@@ -40,7 +48,28 @@ export default function Form() {
           />
           <label htmlFor="checkbox">Remeber me</label>
         </div>
+        <input
+          className="bg-primary-color text-white py-2 rounded-3xl"
+          type="submit"
+          value="Sign in"
+        />
       </form>
+      <Link href="#" className="text-primary-color font-semibold text-sm">
+        Forgot the password?
+      </Link>
+      <div className="flex flex-col items-center">
+        <span className="text-sm">or continue with</span>
+        <div className="flex gap-5 ">
+          <button className="flex items-end gap-1 border py-3 px-8 rounded-xl">
+            <FacebookLogo className="text-2xl text-blue-900 " />
+            <span className="text-sm font-semibold">Facebook</span>
+          </button>
+          <button className="flex items-end gap-1 border py-3 px-8 rounded-xl">
+            <GoogleLogo className="text-2xl text-red-900 " />
+            <span className="text-sm font-semibold">Google</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
